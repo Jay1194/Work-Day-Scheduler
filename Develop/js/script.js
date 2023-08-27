fd33612f94970b93cd6bb28b5866e8a2f4c48f61
Jay <j.thomastt1@gmail.com>
2023-08-27 03:10:22 -0600

    fixed up viewports on grid

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -12,109 +12,25 @@ var checkEvent = function() {
     
     // background color of time blocks automatically changes according to its time
     setInterval(function() {
-
-    // 9am conditions
-    if (9 > hour) {
-      $("#9am").addClass("future");  
-   
-    }if (9 < hour) {
-        $("#9am").addClass("past"); 
-
-    }if (9 == hour) {
-        $("#9am").addClass("present");
-    };
-    
-    // 10am conditions
-    if (10 > hour) {
-        $("#10am").addClass("future");  
       
-        }if (10 < hour) {
-          $("#10am").addClass("past");  
+      // Define the working hours
+      var hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
-        }if (10 == hour) {
-          $("#10am").addClass("present");
-
-      };
-    
-      // 11am conditions
-    if (11 > hour) {
-        $("#11am").addClass("future");  
-    
-        }if (11 < hour) {
-          $("#11am").addClass("past");  
-
-        }if (11 == hour) {
-          $("#11am").addClass("present");
-
-      };
-    
-      // 12am conditions
-      if (12 > hour) {
-        $("#12am").addClass("future");  
-    
-        }if (12 < hour) {
-          $("#12am").addClass("past");  
-
-        }if (12 == hour) {
-
-          $("#12am").addClass("present");
-      };
-    
-      // 1pm conditions
-      if (13 > hour) {
-        $("#1pm").addClass("future");  
-   
-        }if (13 < hour) {
-          $("#1pm").addClass("past"); 
-
-        }if (13 == hour) {
-          $("#1pm").addClass("present");
-          };
-    
-     // 2pm conditions
-     if (14 > hour) {
-        $("#2pm").addClass("future");  
-   
-        }if (14 < hour) {
-          $("#2pm").addClass("past"); 
-        }if (14 == hour) {
-
-          $("#2pm").addClass("present");
-          };
-    
-      // 3pm conditions
-      if (15 > hour) {
-        $("#3pm").addClass("future");  
-   
-        }if (15 < hour) {
-          $("#3pm").addClass("past"); 
-
-        }if (15 == hour) {
-          $("#3pm").addClass("present");
-          };
+      // Get the current hour
+      var currentHour = new Date().getHours();
       
-      // 4pm conditions
-      if (16 > hour) {
-        $("#4pm").addClass("future");  
-   
-        }if (16 < hour) {
-          $("#4pm").addClass("past");     
-          
-        }if (16 == hour) {
-          $("#4pm").addClass("present");
-          };
-      
-      // 5pm conditions
-      if (17 > hour) {
-        $("#5pm").addClass("future");  
-   
-      }if (17 < hour) {
-          $("#5pm").addClass("past");
-          
-      }if (17 == hour) {
-          $("#5pm").addClass("present");
-          
-      };
+      // Loop through each hour
+      for (var i = 0; i < hours.length; i++) {
+        var hour = hours[i];
+        
+        if (hour > currentHour) {
+          $("#" + hour + "am").addClass("future");
+         } else if (hour < currentHour) {
+          $("#" + hour + "am").addClass("past");
+        } else {
+          $("#" + hour + "am").addClass("present");
+        }
+      }
     // if 60 second check
     }, (1000 * 60));
     };
